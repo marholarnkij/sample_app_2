72b58d076c3c6d01c8958cbdd06d46a6c48d7b3a
joe <marholarnkij@hotmail.com>
2012-09-08 23:12:04 +0700

    Add a StaticPages controller

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "GET /static_page" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get static_page_index_path
+      response.status.should be(200)
+    end
+  end
+
+	describe "Home page" do 
+    it "should have the content 'Sample App'" do 
+			visit '/static_page/home'
+			page.should have_content('Sample App')
+		end
+	end
+end
