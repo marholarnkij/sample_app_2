94d7cc26824823723860e83e18cca81671e68209
joe <marholarnkij@hotmail.com>
2012-09-09 00:38:05 +0700

    finish f4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,6 +11,11 @@ describe "StaticPages" do
 			visit '/static_pages/home'
 			page.should have_selector('title',:text => "Ruby on Rails Tutorial Sample App | Home")
 		end
+		
+		it "should not have a custome page title" do 
+			visit '/static_pages/home'
+			page.should_not have_selector('title', :text =>'| Home')
+		end
 	end
 	
 	describe "Help page" do 
